930d752d65739d2994bfd8532f3c9af392ceb2e6
Francin Vincent <admin@francinvincent.no>
2024-02-21 15:23:25 +0100

    feat: add thumbnail information

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,16 @@ export default function RootLayout({ children }: { children: any }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <meta property="og:title" content="Raus - Vi lar ikke tidsklemma vinne!" />
+        <meta property="og:image" content="https://www.raustfellesskap.no/RausThumbnail.png" />
+        <meta
+          property="og:description"
+          content="La oss sammen jobbe for en fremtid der eldre får den oppmerksomheten de fortjener. Nå ønsker vi å tilby tjenester til eldre der helsesektoren ikke strekker til!"
+        />
+        <meta property="og:url" content="https://www.raustfellesskap.no" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="746" />
       </head>
       <body>
         <MantineProvider theme={theme} defaultColorScheme="light">
