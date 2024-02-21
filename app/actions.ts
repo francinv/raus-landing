4cbc03156024d998cf3cd9843b3467ef913a0666
Francin Vincent <admin@francinvincent.no>
2024-02-21 15:07:59 +0100

    feat: add email template

--- app/actions.ts
+++ app/actions.ts
@@ -10,7 +10,40 @@ export async function sendMail(user: MailUser) {
     to: user.email,
     from: 'hei@raustfellesskap.no',
     subject: 'Tusen takk for din registrering!',
-    html: `<strong>Hei ${user.name}!</strong> Tusen takk for at du registrerte deg hos oss.`,
+    html: `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Raus - Vi lar ikke tidsklemma vinne!</title>
+            <style>
+                body { font-family: Arial, sans-serif; margin: 0; padding: 0; }
+                .container { max-width: 600px; margin: auto; padding: 20px; background: #fff; }
+                .content { color: #314528; }
+                .footer { margin-top: 20px; text-align: center; font-size: 12px; color: #aaa; }
+                .signature { margin-top: 20px; color: #314528; }
+            </style>
+            </head>
+            <body>
+            <div class="container">
+                <div class="content">
+                    <h2>Hei!</h2>
+                    <p>Tusen takk for at du viste interesse. Foreløpig er dette kun en markedsundersøkelse for å skape den aller beste løsningen for dere.</p>
+                    <p>Vi gleder oss til dagen vi lanserer! I mellomtiden tar vi gjerne kontakt med deg for å høre hva du kunne tenke deg fra oss.</p>
+                    <p>Vårt løfte er at vi ikke skal gi oss før vi kan tilby noe som dekker deres behov!</p>
+                </div>
+                <div class="signature">
+                    <p>God hilsen,</p>
+                    <p>Francin, Lisa, Renate og Nora i Team Raus</p>
+                    <img src="https://raustfellesskap.no/Logo.png" alt="Raus logo" width="100">
+                </div>
+                <div class="footer">
+                    <p>Dette er en automatisk generert e-post.</p>
+                </div>
+            </div>
+            </body>
+            </html>
+    `,
   };
 
   await mailer
